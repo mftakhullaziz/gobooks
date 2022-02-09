36c1e0b9eb351ae89687aed8ca5a31adb82e9400
Aziz <azizmiftakhul19@gmail.com>
2022-02-09 16:10:07 +0700

    update final auth

--- app.go
+++ app.go
@@ -3,20 +3,26 @@ package main
 import (
 	"github.com/amifth/ApiGo/configuration"
 	"github.com/amifth/ApiGo/controller"
+	"github.com/amifth/ApiGo/middleware"
+	"github.com/amifth/ApiGo/repository"
+	"github.com/amifth/ApiGo/service"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 var (
 	db             *gorm.DB                  = configuration.SetupDatabaseConnection()
-	authController controller.AuthController = controller.NewAuthController()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
 )
 
 func main() {
 	defer configuration.CloseDatabaseConnection(db)
 	r := gin.Default()
 
-	authRoutes := r.Group(("api/auth"))
+	authRoutes := r.Group(("api/auth"), middleware.AuthorizeJWT(jwtService))
 	{
 		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/register", authController.Register)
