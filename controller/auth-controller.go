601bb48e8510f9ffbbd0001e5e276a5d86c48d14
Aziz <azizmiftakhul19@gmail.com>
2022-02-09 15:39:51 +0700

    update controller & repo

--- controller/auth-controller.go
+++ controller/auth-controller.go
@@ -1,8 +1,14 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+
+	"github.com/amifth/ApiGo/dto"
+	"github.com/amifth/ApiGo/entity"
+	"github.com/amifth/ApiGo/helper"
+	"github.com/amifth/ApiGo/service"
+	"github.com/gin-gonic/gin"
 )
 
 type AuthController interface {
@@ -11,17 +17,35 @@ type AuthController interface {
 }
 
 type authController struct {
-
+	authService service.AuthService
+	jwtService  service.JWTService
 }
 
-func NewAuthController() AuthController {
-	return &authController{}
+func NewAuthController(authService service.AuthService, jwtService service.JWTService) AuthController {
+	return &authController{
+		authService: authService,
+		jwtService:  jwtService,
+	}
 }
 
 func (c *authController) Login(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "hello login",
-	})
+	var loginDTO dto.LoginDTO
+	errDTO := ctx.ShouldBind(&loginDTO)
+	if errDTO != nil {
+		response := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+	authResult := c.authService.VerifyCredential(loginDTO.Email, loginDTO.Password)
+	if v, ok := authResult.(entity.User); ok {
+		generatedToken := c.jwtService.GenerateToken(strconv.FormatUint(v.ID, 10))
+		v.Token = generatedToken
+		response := helper.BuildResponse(true, "Ok!", v)
+		ctx.JSON(http.StatusOK, response)
+		return
+	}
+	response := helper.BuildErrorResponse("Please check your credential", "Invalid credential", helper.EmptyObj{})
+	ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 }
 
 func (c *authController) Register(ctx *gin.Context) {
