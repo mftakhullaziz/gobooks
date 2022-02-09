210698a79df2f42d207a3ef829d4e69df9bd6880
Aziz <azizmiftakhul19@gmail.com>
2022-02-09 14:54:57 +0700

    update6

--- service/auth-service.go
+++ service/auth-service.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"github.com/amifth/ApiGo/dto"
+	"github.com/amifth/ApiGo/entity"
+	"github.com/amifth/ApiGo/repository"
+)
+
+type AuthService interface {
+	VerifyCredential(email string, password string) interface{}
+	CreateUser(user dto.UserCreateDTO) entity.User
+	FindByEmail(email string) entity.User
+	IsDuplicateEmail(email string) bool
+}
+
+type authService struct {
+	userRepository repository.UserRepository
+}
+
+func NewAuthService(userRep repository.UserRepository) AuthService {
+	return &authService{
+		userRepository: userRep,
+	}
+}
