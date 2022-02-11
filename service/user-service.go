845ae21ae9f13937946de9749faabca9c9fbd639
Aziz <azizmiftakhul19@gmail.com>
2022-02-11 18:15:11 +0700

    update user service

---
+++ service/user-service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"log"
+
+	"github.com/amifth/ApiGo/dto"
+	"github.com/amifth/ApiGo/entity"
+	"github.com/amifth/ApiGo/repository"
+	"github.com/mashingan/smapping"
+)
+
+type UserService interface {
+	Update(user dto.UserUpdateDTO) entity.User
+	Profile(userID string) entity.User
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{
+		userRepository: userRepo,
+	}
+}
+
+func (service *userService) Update(user dto.UserUpdateDTO) entity.User {
+	userToUpdate := entity.User{}
+	err := smapping.FillStruct(&userToUpdate, smapping.MapFields(user))
+	if err != nil {
+		log.Fatalf("Failed map %v:", err)
+	}
+	updateUser := service.userRepository.UpdateUser(userToUpdate)
+	return updateUser
+}
+
+func (service *userService) Profile(userID string) entity.User {
+	return service.userRepository.ProfileUser(userID)
+}
