0f704213e16ec29ab1059f23b5614879bc81876c
Aziz <azizmiftakhul19@gmail.com>
2022-02-15 04:42:17 +0700

    update

---
+++ controller/book-controller.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/amifth/ApiGo/entity"
+	"github.com/amifth/ApiGo/helper"
+	"github.com/amifth/ApiGo/service"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+type BookController interface {
+	All(context *gin.Context)
+	FindByID(context *gin.Context)
+	Insert(context *gin.Context)
+	Update(context *gin.Context)
+	Delete(context *gin.Context)
+}
+
+type bookController struct {
+	bookService service.BookService
+	jwtService  service.JWTService
+}
+
+func NewBookController(bookServ service.BookService, jwtServ service.JWTService) BookController {
+	return &bookController{
+		bookService: bookServ,
+		jwtService:  jwtServ,
+	}
+}
+
+func (c *bookController) All(context *gin.Context) {
+	var books []entity.Book = c.bookService.All()
+	res := helper.BuildResponse(true, "OK!", books)
+	context.JSON(http.StatusOK, res)
+}
+
+func (c *bookController) FindByID(context *gin.Context) {
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		res := helper.BuildErrorResponse("No param id was found", err.Error(), helper.EmptyObj{})
+		context.AbortWithStatusJSON(http.StatusBadRequest, res)
+		return
+	}
+	var books entity.Book = c.bookService.FindByID(id)
+	if (books == entity.Book{}) {
+		res := helper.BuildErrorResponse("Data not found", "no data given id", helper.EmptyObj{})
+		context.JSON(http.StatusNotFound, res)
+	} else {
+		res := helper.BuildResponse(true, "OK!", books)
+		context.JSON(http.StatusOK, res)
+	}
+}
+
+func (c *bookController) getUserIDByToken(token string) string {
+	uToken, err := c.jwtService.ValidateToken(token)
+	if err != nil {
+		panic(err.Error())
+	}
+	claims := uToken.Claims.(jwt.MapClaims)
+	return fmt.Sprint("%v", claims["user_id"])
+}
