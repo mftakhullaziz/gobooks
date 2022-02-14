0294f1fffbf1cf052c0feb8138f31db39d72d442
Aziz <azizmiftakhul19@gmail.com>
2022-02-15 04:02:58 +0700

    update

--- repository/book-repository.go
+++ repository/book-repository.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	"github.com/amifth/ApiGo/entity"
+	"gorm.io/gorm"
+)
+
+type BookRepository interface {
+	InsertBook(b entity.Book) entity.Book
+	UpdateBook(b entity.Book) entity.Book
+	DeleteBook(b entity.Book)
+	AllBook() []entity.Book
+	FindBookByID(bookID uint64) entity.Book
+}
+
+type bookConnection struct {
+	connection *gorm.DB
+}
+
+func NewBookRepository(dbConnection *gorm.DB) BookRepository {
+	return &bookConnection{
+		connection: dbConnection,
+	}
+}
+
+func (db *bookConnection) InsertBook(b entity.Book) entity.Book {
+	db.connection.Save(&b)
+	db.connection.Preload("user").Find(&b)
+	return b
+}
+
+func (db *bookConnection) UpdateBook(b entity.Book) entity.Book {
+	db.connection.Save(&b)
+	db.connection.Preload("user").Find(&b)
+	return b
+}
