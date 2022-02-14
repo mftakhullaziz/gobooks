d3e132f588d15d3500abf6dbe27015ae752a685b
Aziz <azizmiftakhul19@gmail.com>
2022-02-15 05:04:59 +0700

    update service

--- controller/book-controller.go
+++ controller/book-controller.go
@@ -75,11 +75,65 @@ func (c *bookController) Insert(context *gin.Context) {
 	}
 }
 
+func (c *bookController) Update(context *gin.Context) {
+	var bookUpdateDTO dto.BookUpdateDTO
+	errDTO := context.ShouldBind(&bookUpdateDTO)
+	if errDTO != nil {
+		res := helper.BuildErrorResponse("Request failed", errDTO.Error(), helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, res)
+		return
+	}
+	authHeader := context.GetHeader("Authorization")
+	token, errToken := c.jwtService.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := fmt.Sprintf("%v", claims["user_id"])
+	if c.bookService.IsAllowedToEdit(userID, bookUpdateDTO.ID) {
+		id, errID := strconv.ParseUint(userID, 10, 64)
+		if errID == nil {
+			bookUpdateDTO.UserID = id
+		}
+		result := c.bookService.Update(bookUpdateDTO)
+		response := helper.BuildResponse(true, "OK!", result)
+		context.JSON(http.StatusOK, response)
+	} else {
+		response := helper.BuildErrorResponse("You dont have permission", "Permission denied", helper.EmptyObj{})
+		context.JSON(http.StatusForbidden, response)
+	}
+}
+
+func (c *bookController) Delete(context *gin.Context) {
+	var book entity.Book
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		response := helper.BuildErrorResponse("Failed get id", "No Param id were found", helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, response)
+	}
+	book.ID = id
+	authHeader := context.GetHeader("Authorization")
+	token, errToken := c.jwtService.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := fmt.Sprintf("%v", claims["user_id"])
+	if c.bookService.IsAllowedToEdit(userID, book.ID) {
+		c.bookService.Delete(book)
+		res := helper.BuildResponse(true, "Delete Success", helper.EmptyObj{})
+		context.JSON(http.StatusOK, res)
+	} else {
+		response := helper.BuildErrorResponse("You dont have permission", "Permission denied", helper.EmptyObj{})
+		context.JSON(http.StatusForbidden, response)
+	}
+}
+
 func (c *bookController) getUserIDByToken(token string) string {
 	uToken, err := c.jwtService.ValidateToken(token)
 	if err != nil {
 		panic(err.Error())
 	}
 	claims := uToken.Claims.(jwt.MapClaims)
-	return fmt.Sprint("%v", claims["user_id"])
+	return fmt.Sprintf("%v", claims["user_id"])
 }
