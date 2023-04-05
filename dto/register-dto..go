4dd0d4c6ee3da0abe8dc1fad40df083e486616eb
amifth <miftakhul.14116078@student.itera.ac.id>
2023-04-06 03:58:39 +0700

    feat: refactor

--- dto/register-dto..go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type RegisterDTO struct {
-	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required,email"`
-	Password string `json:"password" form:"password" binding:"required"`
-}
---
+++ dto/user_dto.go
@@ -0,0 +1,14 @@
+package dto
+
+type UserUpdateDTO struct {
+	ID       uint64 `json:"id" form:"id"`
+	Name     string `json:"name" form:"name" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password,omitempty" form:"password,omitempty"`
+}
+
+// type UserCreateDTO struct {
+// 	Name     string `json:"name" form:"name" binding:"required"`
+// 	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
+// 	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6"`
+// }
